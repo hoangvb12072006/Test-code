360e0d1fa1ef8947ab1a959c5b9ab0b752542d8d
hoangvb12072006 <hoangvb12072006@gmail.com>
2026-02-14 14:50:51 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,15 +1,15 @@
 // Đợi trang load xong
 document.addEventListener('DOMContentLoaded', () => {
-    // Lấy URL hiện tại để gán class 'active' cho nút tương ứng
+    // Lấy URL hiện tại
     const currentLocation = location.href;
-    const menuItem = document.querySelectorAll('.nav-btn');
-    const menuLength = menuItem.length;
+    const menuItems = document.querySelectorAll('.nav-btn');
 
-    for (let i = 0; i < menuLength; i++) {
-        if (menuItem[i].href === currentLocation) {
-            menuItem[i].classList.add("active");
+    // Lặp qua các nút trên menu, nếu link khớp với trang hiện tại thì thêm class 'active'
+    menuItems.forEach(item => {
+        if (currentLocation.includes(item.getAttribute('href'))) {
+            item.classList.add("active");
         } else {
-            menuItem[i].classList.remove("active");
+            item.classList.remove("active");
         }
-    }
+    });
 });
